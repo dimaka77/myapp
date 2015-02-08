83a9e4e72b61a03bdbd81f2c27d4ee79a104b9ff
DR <dimaka77@gmail.com>
2015-02-07 23:25:44 -0800

    updated app layout

--- app/assets/javascripts/backbone/app.js
+++ app/assets/javascripts/backbone/app.js
@@ -1,20 +1,22 @@
 
-var app = app || {}; 
-var ENTER_KEY = 13;
+// var app = app || {}; 
+// var ENTER_KEY = 13;
 
-#= require_self
-#= require_tree ./templates
-#= require_tree ./models
-#= require_tree ./views
-#= require_tree ./routers
+// #= require_self
+// #= require_tree ./templates
+// #= require_tree ./models
+// #= require_tree ./views
+// #= require_tree ./routers
+// #= require_tree ./collections
 
-window.Myapp =
-  Models: {}
-  Collections: {}
-  Routers: {}
-  Views: {}
+// window.Myapp =
+//   Models: {}
+//   Collections: {}
+//   Routers: {}
+//   Views: {}
+//   Collections: {}
   
-$(function() {
-// Kick things off by creating the **App**.
-new app.AppView(); 
-});
+// $(function() {
+// // Kick things off by creating the **App**.
+// new app.AppView(); 
+// });
